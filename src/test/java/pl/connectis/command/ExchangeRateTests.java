2c3b1baa30079f545d620c80bb94690f1b10b151
Grzegorz Witkowski <grisza.karamazow@gmail.com>
2020-04-03 19:20:57 +0200

    Moves tests from MainTests to ExchangeRateTests

--- src/test/java/pl/connectis/command/ExchangeRateTests.java
+++ src/test/java/pl/connectis/command/ExchangeRateTests.java
@@ -1,27 +1,26 @@
-package pl.connectis;
+package pl.connectis.command;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import picocli.CommandLine;
-import pl.connectis.command.ExchangeRate;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(Lifecycle.PER_CLASS)
-public class MainTests {
+public class ExchangeRateTests {
 
-    private CommandLine commandLine;
+    private final CommandLine commandLine = new CommandLine(new ExchangeRate());
+    private final StringWriter stringWriter = new StringWriter();
 
     @BeforeAll
     public void initializeCommandLine() {
-        commandLine = new CommandLine(new ExchangeRate());
         commandLine.setCaseInsensitiveEnumValuesAllowed(true);
-        StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         commandLine.setOut(printWriter);
     }
@@ -44,6 +43,7 @@ public class MainTests {
     @Test
     public void testUnsupportedArgument() {
         assertEquals(2, commandLine.execute("test"));
+        assertTrue(stringWriter.toString().contains("Unmatched argument at index 0: 'test'"));
     }
 
 }
